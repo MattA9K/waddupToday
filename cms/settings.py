15caefa6073d87cf6c424dde863daf3160cd3f04
CiniCraft <CiniCraftMatt@gmail.com>
2016-03-14 16:23:19 -0700

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -112,8 +112,8 @@ CONTEXT_PROCESSORS = (
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cinicraftmatt@gmail.com'
-EMAIL_HOST_PASSWORD = 'jadzia00'
+EMAIL_HOST_USER = 'ceeeet@gmail.com'
+EMAIL_HOST_PASSWORD = '123123'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'user@domain.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
